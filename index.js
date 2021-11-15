e7871fd71849bc30f4178e4d29b31c63db3b49e0
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-11-15 15:14:59 +0000

    Finsihed code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//write your code here
+// //write your code here
+// function findLongestWord(words){
+//  let longestWord = ""
+//  for (let i = 0; i < words.length; i++){
+//      if (longestWord)
+//  }
+// }
+const findLongestWord = (words) => {
+    let longestWord = ""
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word
+        }
+    }
+    return longestWord
+}
+
+console.log(findLongestWord(['OhGod', 'damnnnnnnn', 'jeezzzusss', 'ball', 'cooon']))
